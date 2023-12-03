ce6ec3be50c036dc1a9d910b57483da6017260ad
S Blanco <sblan-co>
2023-12-03 13:30:47 +0000

    Initial commit

---
+++ src/test/kotlin/com/sblanco/QuoteRetrieverTest.kt
@@ -0,0 +1,19 @@
+package com.sblanco
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class QuoteRetrieverTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
